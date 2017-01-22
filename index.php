d59cb5adf703f646316814419f4d4220ada81aac
gtvtortst <hav3pochta@ukr.net>
2017-01-22 08:53:08 +0200

    ad commit to code

--- index.php
+++ index.php
@@ -1,8 +1,42 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: andrey
- * Date: 22.01.17
- * Time: 8:33
- */
-echo phpinfo();
+
+// Целевой интерфейс, клиент умеет работать только с ним
+interface iTarget
+{
+    public function query();
+}
+
+// Адаптируемый интерфейс. Клиент с ним не умеет работать, но очень хочет
+interface iAdaptee
+{
+    public function request();
+}
+
+
+// Класс, реализующий адаптирумым интерфейс
+class Adaptee implements iAdaptee
+{
+    public function request()
+    {
+        return __CLASS__ . "::" . __METHOD__;
+    }
+}
+
+class Adapter implements iTarget
+{
+    protected
+        $adaptee = null;
+
+    public function __construct()
+    {
+        $this -> adaptee = new Adaptee();
+    }
+
+    public function query()
+    {
+        return $this -> adaptee -> request();
+    }
+}
+
+$Target = new Adapter();
+print $Target -> query(); // "Adaptee::request"
